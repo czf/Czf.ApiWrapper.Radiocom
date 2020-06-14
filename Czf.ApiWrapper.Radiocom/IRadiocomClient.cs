edc63d9b937939b9a7674a7d53f41e49155bf4ca
czf <fingarc@gmail.com>
2020-06-13 23:19:13 -0700

    version 1.1.1: correcting comment for day of week parameter.  Note that the UI for requests for play history doesn't convert day of week to UTC which results in weird behavior. include snpkg on pack

--- Czf.ApiWrapper.Radiocom/IRadiocomClient.cs
+++ Czf.ApiWrapper.Radiocom/IRadiocomClient.cs
@@ -1,16 +1,16 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 
 namespace Czf.ApiWrapper.Radiocom
 {
-    public interface IRadiocomClient
+    public interface IRadiocomClient 
     {
         /// <summary>
         /// Retrieve recently played information for a specific station.
         /// </summary>
         /// <param name="stationId">the radio.com station Id</param>
         /// <param name="hour">the hour of the station converted from its local time to UTC</param>
-        /// <param name="dayOfWeek">the day of the week for the station using it local time</param>
-        /// <example> wednesday at 10pm in pacific daylight time would use dayOfWeek = 3 and go offset seven hours so hour = 5</example>
+        /// <param name="dayOfWeek">the day of the week</param>
         Task<StationRecentlyPlayedResponse> StationRecentlyPlayed(int stationId, int hour, DayOfWeek dayOfWeek  /*string category, bool ignore_resolve_media*/);
     }
 }
--- Czf.ApiWrapper.Radiocom/RadiocomClient.cs
+++ Czf.ApiWrapper.Radiocom/RadiocomClient.cs
@@ -32,8 +32,7 @@ namespace Czf.ApiWrapper.Radiocom
         /// </summary>
         /// <param name="stationId">the radio.com station Id</param>
         /// <param name="hour">the hour of the station converted from its local time to UTC</param>
-        /// <param name="dayOfWeek">the day of the week for the station using it local time</param>
-        /// <example> wednesday at 10pm in pacific daylight time would use dayOfWeek = 3 and go offset seven hours so hour = 5</example>
+        /// <param name="dayOfWeek">the day of the week for the station converted from its local time to UTC</param>
         public async Task<StationRecentlyPlayedResponse> StationRecentlyPlayed(int stationId, int hour, DayOfWeek dayOfWeek  /*string category, bool ignore_resolve_media*/)
         {
             string query = string.Format(STATION_RECENTLY_PLAYED_PATH, stationId, hour, (int)dayOfWeek);
